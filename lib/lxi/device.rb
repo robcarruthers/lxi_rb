6bb36d2aae7788866f2f247502b292340b57ac1b
Rob Carruthers <40727678+robcarruthers@users.noreply.github.com>
2023-05-06 18:47:14 +1200

    chore: add github CI yml

--- lib/lxi/device.rb
+++ lib/lxi/device.rb
@@ -18,19 +18,24 @@ module Lxi
       @id = Lxi.lxi_connect(@address, @port, @name, @timeout, @protocol)
       @id
     end
+    method_alias :open, :connect
 
     def disconnect
       Lxi.lxi_disconnect(@device_id)
     end
+    method_alias :close, :disconnect
 
-    def send_scpi(message)
+    def send(message)
       Lxi.lxi_send(@device_id, message, message.length, @timeout)
     end
+    method_alias :scpi, :send
+    method_alias :write, :send
 
     def read(length)
       message = FFI::MemoryPointer.new(:char, length)
       Lxi.lxi_receive(@id, message, length, @timeout)
       message.read_string
     end
+    method_alias :gets, :read
   end
 end
