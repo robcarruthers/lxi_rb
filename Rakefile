fe0c73d84d8cb5b17519a0bcda4c114603b274e8
Rob Carruthers <40727678+robcarruthers@users.noreply.github.com>
2023-05-08 10:05:26 +1200

    refactor:(Lxi module) Breakout into mixins

--- Rakefile
+++ Rakefile
@@ -2,6 +2,7 @@
 
 require 'bundler/gem_tasks'
 require 'rake/testtask'
+require 'structured_changelog/tasks'
 
 Rake::TestTask.new(:test) do |t|
   t.libs << 'test'
---
+++ lib/lxi/discovery.rb
@@ -0,0 +1,38 @@
+module Lxi
+  module Discovery
+    module_function
+
+    # Search for LXI-11 devices on the specified interface and return hash of devices
+    def devices(timeout: 1000, type: :vxi11)
+      raise Error, 'LXI Library Initialisation Error' unless lxi_init == LXI_OK
+
+      devices = []
+      device_callback =
+        FFI::Function.new(:void, %i[pointer pointer]) do |address, id|
+          devices << { address: address.read_string, id: id.read_string }
+        end
+
+      info = LxiInfo.new
+      info[:device] = device_callback
+
+      lxi_discover_internal(info, timeout, type)
+      sleep 0.1
+      devices
+    end
+
+    # Discover LXI-11 devices on the LAN
+    def discover_local(timeout: 1000, type: :vxi11)
+      Lxi.init_lxi_session
+
+      info = LxiInfo.new
+      info[:broadcast] = BroadcastCallback
+      info[:device] = DeviceCallback
+
+      puts "Searching for LXI devices - please wait...\n\n"
+
+      result = lxi_discover_internal(info, timeout, type)
+
+      puts "Error during discovery: #{result}" if result.negative?
+    end
+  end
+end
